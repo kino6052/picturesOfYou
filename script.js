4854bdb3f81e4d4619bb7378d40a91686932b974
kino6052 <113693383+nino-ux@users.noreply.github.com>
2025-01-08 21:58:29 +0300

    Refactor

---
+++ script.js
@@ -0,0 +1,51 @@
+const pug = require("pug");
+
+// Compile the Pug template to a function
+function pugToHtml(pugTemplate) {
+  try {
+    // Compile the template
+    const compiledFunction = pug.compile(pugTemplate);
+
+    // Render the HTML
+    const html = compiledFunction();
+
+    return html;
+  } catch (err) {
+    console.error("Error converting Pug to HTML:", err);
+    return null;
+  }
+}
+
+// Example usage:
+// const pugTemplate = `
+//   html
+//     head
+//       title My Site
+//     body
+//       h1 Welcome
+//       p This is my site
+// `;
+// const html = pugToHtml(pugTemplate);
+
+const fs = require("fs");
+
+function convertIndexPugToHtml() {
+  try {
+    // Read the index.pug file
+    const pugTemplate = fs.readFileSync("./index.pug", "utf8");
+
+    // Convert to HTML using pugToHtml function
+    const html = pugToHtml(pugTemplate);
+
+    if (html) {
+      // Write the HTML to index.html
+      fs.writeFileSync("./index.html", html);
+      console.log("Successfully converted index.pug to index.html");
+    }
+  } catch (err) {
+    console.error("Error converting index.pug:", err);
+  }
+}
+
+// Run the conversion
+convertIndexPugToHtml();
