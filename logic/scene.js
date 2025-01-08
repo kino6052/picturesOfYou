8dcc6a983536518fa69c2ec0c8541fc7df620299
kino6052 <113693383+nino-ux@users.noreply.github.com>
2025-01-08 10:26:22 +0300

    Update

--- logic/scene.js
+++ logic/scene.js
@@ -1,232 +1,273 @@
-// function drawScene() {
-//   setupFramebuffer();
+/**
+ * Helper function to bind buffers and set up attributes.
+ */
+function bindBuffersAndAttributes({
+  positionBuffer,
+  normalBuffer,
+  textureCoordBuffer,
+  program,
+}) {
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.vertexAttribPointer(program.aVertexPosition, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(program.aVertexPosition);
 
-//   renderSceneToFramebuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+  gl.vertexAttribPointer(program.aVertexNormal, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(program.aVertexNormal);
 
-//   renderFramebufferToScreen();
+  if (textureCoordBuffer) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+    gl.vertexAttribPointer(program.aTextureCoord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(program.aTextureCoord);
+  }
+}
 
-//   updateAffineTransformationsArray();
-// }
+function initializeFramebuffer() {
+  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+  gl.useProgram(prg);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearDepth(100.0);
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LEQUAL);
+  gl.viewport(0, 0, 2048, 2048);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  mat4.perspective(45, 2048 / 2048, 0.1, 10000.0, pMatrix);
+}
 
-// /**
-//  * Sets up the framebuffer and initializes WebGL state.
-//  */
-// function setupFramebuffer() {
-//   gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
-//   gl.useProgram(prg);
-//   gl.clearColor(0.0, 0.0, 0.0, 1.0);
-//   gl.clearDepth(100.0);
-//   gl.enable(gl.DEPTH_TEST);
-//   gl.depthFunc(gl.LEQUAL);
-//   gl.viewport(0, 0, 2048, 2048);
-//   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-//   mat4.perspective(45, 2048 / 2048, 0.1, 10000.0, pMatrix);
-//   setupCamera();
-// }
+function setupViewMatrix() {
+  mat4.identity(mvMatrix);
+  mat4.translate(mvMatrix, [0.0, -1.3, -3.0]);
+  mat4.rotate(mvMatrix, (-3.14 * 29) / 180 + mouseY / 10000, [1, 0, 0]);
+  mat4.rotate(mvMatrix, mouseX / 10000, [0, 1, 0]);
+}
 
-// /**
-//  * Sets up the camera transformation.
-//  */
-// function setupCamera() {
-//   mat4.identity(mvMatrix);
-//   mat4.translate(mvMatrix, [0.0, -1.3, -3.0]);
-//   mat4.rotate(mvMatrix, (-3.14 * 29) / 180 + mouseY / 10000, [1, 0, 0]);
-//   mat4.rotate(mvMatrix, mouseX / 10000, [0, 1, 0]);
-// }
+function drawVideoScreen() {
+  mvPushMatrix();
+  mat4.translate(mvMatrix, [-0.0, 0.65, 0.49]);
+  mat4.rotate(mvMatrix, (3.14 * 30) / 180, [0, 1, 0]);
+  mat4.scale(mvMatrix, [1.4, 0.95, 0.0]);
 
-// /**
-//  * Renders the main scene to the framebuffer.
-//  */
-// function renderSceneToFramebuffer() {
-//   drawScreen();
-//   drawBackground();
-//   drawRain();
-//   drawTV();
-//   finalizeFramebufferRendering();
-// }
+  bindBuffersAndAttributes({
+    positionBuffer: screenVerticesBuffer,
+    normalBuffer: screenNormalsBuffer,
+    textureCoordBuffer: screenTextureCoordBuffer,
+    program: prg,
+  });
 
-// /**
-//  * Draws the textured screen.
-//  */
-// function drawScreen() {
-//   prepareObject([-0.0, 0.65, 0.49], (3.14 * 30) / 180, [1.4, 0.95, 0.0]);
-//   bindBuffers(
-//     screenVerticesBuffer,
-//     screenNormalsBuffer,
-//     screenTextureCoordBuffer,
-//     prg
-//   );
-//   bindTexture(videoTexture, videoready ? video : null);
-//   drawElements(screenIndicesBuffer, 6);
-// }
+  gl.uniform1i(prg.uIsTextureEnabled, true);
 
-// /**
-//  * Draws the background.
-//  */
-// function drawBackground() {
-//   prepareObject([0, 4, -4], (3.14 * 29) / 180, [20, 10, 10]);
-//   bindBuffers(
-//     screenVerticesBuffer,
-//     screenNormalsBuffer,
-//     screenTextureCoordBuffer,
-//     prg
-//   );
-//   bindTexture(backgroundTexture, backgroundTexture.image);
-//   drawElements(screenIndicesBuffer, 6);
-// }
+  if (videoTexture) {
+    gl.bindTexture(gl.TEXTURE_2D, videoTexture);
+  }
 
-// /**
-//  * Draws the rain.
-//  */
-// function drawRain() {
-//   [polaroidTexture00, polaroidTexture01].forEach((texture, index) => {
-//     bindTexture(texture, texture.image);
-//     setupTextureParameters();
-//     renderDroplets(
-//       (index * dropletQuantity) / 4,
-//       ((index + 1) * dropletQuantity) / 4
-//     );
-//   });
-// }
+  if (videoready) {
+    try {
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, video);
+    } catch (e) {}
+  }
 
-// /**
-//  * Renders droplets for rain effects.
-//  */
-// function renderDroplets(start, end) {
-//   for (let i = start; i < end; i++) {
-//     const transforms = affineTransformationsArray[i];
-//     prepareObject(
-//       [transforms[3], transforms[4], transforms[5]],
-//       transforms.slice(0, 3),
-//       [0.3, 0.3, 0.3]
-//     );
-//     bindBuffers(
-//       screenVerticesBuffer,
-//       screenNormalsBuffer,
-//       screenTextureCoordBuffer,
-//       prg
-//     );
-//     drawElements(screenIndicesBuffer, 6);
-//   }
-// }
+  gl.uniformMatrix4fv(prg.uMVMatrix, false, mvMatrix);
+  gl.uniformMatrix4fv(prg.uPMatrix, false, pMatrix);
 
-// /**
-//  * Draws the TV model.
-//  */
-// function drawTV() {
-//   prepareObject([0, 0, 0], (3.14 * 30) / 180, [0.6, 0.6, 0.6]);
-//   bindBuffers(verticesBuffer, tvNormalsBuffer, null, prg);
-//   disableTexture();
-//   drawElements(indicesBuffer, tv.indices.length);
-// }
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, screenIndicesBuffer);
+  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
 
-// /**
-//  * Finalizes framebuffer rendering.
-//  */
-// function finalizeFramebufferRendering() {
-//   mat4.set(mvMatrix, nMatrix);
-//   mat4.inverse(nMatrix);
-//   mat4.transpose(nMatrix);
-//   gl.uniformMatrix4fv(prg.uNMatrix, false, nMatrix);
-//   gl.flush();
-//   gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-// }
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
-// /**
-//  * Renders the framebuffer to the screen.
-//  */
-// function renderFramebufferToScreen() {
-//   gl.useProgram(postPrg);
-//   gl.clearColor(0.0, 0.0, 0.0, 1.0);
-//   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-//   gl.viewport(0, 0, c_width, c_height);
-//   bindBuffers(
-//     screenVerticesBuffer,
-//     screenNormalsBuffer,
-//     screenTextureCoordBuffer,
-//     postPrg
-//   );
-//   bindTexture(framebufferTexture);
-//   mat4.identity(postMatrix);
-//   gl.uniformMatrix4fv(postPrg.uMVMatrix, false, postMatrix);
-//   gl.uniform1f(postPrg.uTime, time);
-//   drawElements(screenIndicesBuffer, 6);
-//   gl.flush();
-// }
+  mvPopMatrix();
+  gl.bindTexture(gl.TEXTURE_2D, null);
+}
 
-// /**
-//  * Prepares the transformation matrix for an object.
-//  */
-// function prepareObject(translation, rotation, scale) {
-//   mvPushMatrix();
-//   mat4.translate(mvMatrix, translation);
-//   if (Array.isArray(rotation)) {
-//     rotation.forEach((angle, i) =>
-//       mat4.rotate(mvMatrix, angle, [i === 0, i === 1, i === 2])
-//     );
-//   } else {
-//     mat4.rotate(mvMatrix, rotation, [0, 1, 0]);
-//   }
-//   mat4.scale(mvMatrix, scale);
-// }
+function drawBackground() {
+  mvPushMatrix();
+  mat4.translate(mvMatrix, [0, 4, -4]);
+  mat4.rotate(mvMatrix, (3.14 * 29) / 180, [1, 0, 0]);
+  mat4.scale(mvMatrix, [20, 10, 10]);
 
-// /**
-//  * Binds vertex buffers and attributes for rendering.
-//  */
-// function bindBuffers(vertexBuffer, normalBuffer, textureBuffer, program) {
-//   if (vertexBuffer) {
-//     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-//     gl.vertexAttribPointer(program.aVertexPosition, 3, gl.FLOAT, false, 0, 0);
-//   }
-//   if (normalBuffer) {
-//     gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-//     gl.vertexAttribPointer(program.aVertexNormal, 3, gl.FLOAT, false, 0, 0);
-//   }
-//   if (textureBuffer) {
-//     gl.enableVertexAttribArray(program.aTextureCoord);
-//     gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
-//     gl.vertexAttribPointer(program.aTextureCoord, 2, gl.FLOAT, false, 0, 0);
-//   }
-// }
+  bindBuffersAndAttributes({
+    positionBuffer: screenVerticesBuffer,
+    normalBuffer: screenNormalsBuffer,
+    textureCoordBuffer: screenTextureCoordBuffer,
+    program: prg,
+  });
 
-// /**
-//  * Binds a texture and optionally uploads new data.
-//  */
-// function bindTexture(texture, data = null) {
-//   if (texture) {
-//     gl.bindTexture(gl.TEXTURE_2D, texture);
-//     if (data) {
-//       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, data);
-//     }
-//   }
-// }
+  gl.uniform1i(prg.uIsTextureEnabled, true);
 
-// /**
-//  * Sets texture parameters for filtering and mipmaps.
-//  */
-// function setupTextureParameters() {
-//   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-//   gl.texParameteri(
-//     gl.TEXTURE_2D,
-//     gl.TEXTURE_MIN_FILTER,
-//     gl.LINEAR_MIPMAP_NEAREST
-//   );
-//   gl.generateMipmap(gl.TEXTURE_2D);
-// }
+  gl.bindTexture(gl.TEXTURE_2D, backgroundTexture);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGB,
+    gl.RGB,
+    gl.UNSIGNED_BYTE,
+    backgroundTexture.image
+  );
 
-// /**
-//  * Disables the texture for rendering.
-//  */
-// function disableTexture() {
-//   gl.uniform1i(prg.uIsTextureEnabled, false);
-// }
+  gl.uniformMatrix4fv(prg.uMVMatrix, false, mvMatrix);
+  gl.uniformMatrix4fv(prg.uPMatrix, false, pMatrix);
 
-// /**
-//  * Draws elements using the provided index buffer.
-//  */
-// function drawElements(indexBuffer, count) {
-//   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-//   gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, 0);
-//   gl.bindBuffer(gl.ARRAY_BUFFER, null);
-//   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-// }
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, screenIndicesBuffer);
+  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  mvPopMatrix();
+  gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+function setupRainTexture(texture) {
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGB,
+    gl.RGB,
+    gl.UNSIGNED_BYTE,
+    texture.image
+  );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(
+    gl.TEXTURE_2D,
+    gl.TEXTURE_MIN_FILTER,
+    gl.LINEAR_MIPMAP_NEAREST
+  );
+  gl.generateMipmap(gl.TEXTURE_2D);
+}
+
+function drawRainDroplets(startIndex, endIndex) {
+  for (let i = startIndex; i < endIndex; i++) {
+    mvPushMatrix();
+    mat4.translate(mvMatrix, [
+      affineTransformationsArray[i][3],
+      affineTransformationsArray[i][4],
+      affineTransformationsArray[i][5],
+    ]);
+    mat4.rotate(mvMatrix, affineTransformationsArray[i][0], [1, 0, 0]);
+    mat4.rotate(mvMatrix, affineTransformationsArray[i][1], [0, 1, 0]);
+    mat4.rotate(mvMatrix, affineTransformationsArray[i][2], [0, 0, 1]);
+    mat4.scale(mvMatrix, [0.3, 0.3, 0.3]);
+
+    bindBuffersAndAttributes({
+      positionBuffer: screenVerticesBuffer,
+      normalBuffer: screenNormalsBuffer,
+      textureCoordBuffer: screenTextureCoordBuffer,
+      program: prg,
+    });
+
+    gl.uniformMatrix4fv(prg.uMVMatrix, false, mvMatrix);
+    gl.uniformMatrix4fv(prg.uPMatrix, false, pMatrix);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, screenIndicesBuffer);
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+    mvPopMatrix();
+  }
+}
+
+function drawRainEffect() {
+  setupRainTexture(polaroidTexture00);
+  drawRainDroplets(0, dropletQuantity / 4);
+  gl.bindTexture(gl.TEXTURE_2D, null);
+
+  setupRainTexture(polaroidTexture01);
+  drawRainDroplets(dropletQuantity / 4, dropletQuantity / 2);
+  gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+function drawTVModel() {
+  gl.disableVertexAttribArray(prg.aTextureCoord);
+  mvPushMatrix();
+  mat4.translate(mvMatrix, [0, 0, 0]);
+  mat4.rotate(mvMatrix, (3.14 * 30) / 180, [0, 1, 0]);
+  mat4.scale(mvMatrix, [0.6, 0.6, 0.6]);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+  gl.vertexAttribPointer(prg.aVertexPosition, 3, gl.FLOAT, false, 0, 0);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, tvNormalsBuffer);
+  gl.vertexAttribPointer(prg.aVertexNormal, 3, gl.FLOAT, false, 0, 0);
+
+  gl.uniformMatrix4fv(prg.uMVMatrix, false, mvMatrix);
+  gl.uniformMatrix4fv(prg.uPMatrix, false, pMatrix);
+
+  gl.uniform1i(prg.uIsTextureEnabled, false);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+  gl.drawElements(gl.TRIANGLES, tv.indices.length, gl.UNSIGNED_SHORT, 0);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  mat4.set(mvMatrix, nMatrix);
+  mat4.inverse(nMatrix);
+  mat4.transpose(nMatrix);
+
+  gl.uniformMatrix4fv(prg.uNMatrix, false, nMatrix);
+  gl.flush();
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+}
+
+function applyPostProcessing() {
+  gl.useProgram(postPrg);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearDepth(100.0);
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LEQUAL);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.viewport(0, 0, c_width, c_height);
+  gl.enableVertexAttribArray(postPrg.aTextureCoord);
+
+  mat4.identity(postMatrix);
+
+  gl.uniformMatrix4fv(postPrg.uMVMatrix, false, postMatrix);
+  gl.uniformMatrix4fv(postPrg.uPMatrix, false, pMatrix);
+  gl.uniform1f(postPrg.uTime, time);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, screenVerticesBuffer);
+  gl.vertexAttribPointer(postPrg.aVertexPosition, 3, gl.FLOAT, false, 0, 0);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, screenNormalsBuffer);
+  gl.vertexAttribPointer(postPrg.aVertexNormal, 3, gl.FLOAT, false, 0, 0);
+
+  gl.uniform1i(postPrg.uIsTextureEnabled, true);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, screenTextureCoordBuffer);
+  gl.vertexAttribPointer(postPrg.aTextureCoord, 2, gl.FLOAT, false, 0, 0);
+
+  gl.bindTexture(gl.TEXTURE_2D, framebufferTexture);
+
+  gl.uniformMatrix4fv(postPrg.uMVMatrix, false, mvMatrix);
+  gl.uniformMatrix4fv(postPrg.uPMatrix, false, pMatrix);
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, screenIndicesBuffer);
+  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  gl.flush();
+}
+
+/**
+ * Main rendering function. Called every 500ms according to WebGLStart function
+ */
+function drawScene() {
+  try {
+    initializeFramebuffer();
+    setupViewMatrix();
+
+    drawVideoScreen();
+    drawBackground();
+    drawRainEffect();
+    drawTVModel();
+    applyPostProcessing();
+  } catch (err) {
+    alert(err);
+  }
+
+  updateAffineTransformationsArray();
+}
